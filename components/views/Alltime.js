2d6a8bb6cdddcd065a4e2541d8304a1f80434604
JosefHutton <josefdhutton@gmail.com>
2023-12-21 17:47:17 -0500

    revert commit
    This reverts commit 152bc0003a513414ddd3325b95ca5efe50b5f2cc.

--- components/views/Alltime.js
+++ components/views/Alltime.js
@@ -3,34 +3,33 @@ import html from "html-literal";
 export default (state) => html`
 <section id="Alltime">
   <h1>All Time Most Played</h1>
-  <h3>If you are looking for some of the most popular games that Steam has, take a look at our list of the Top 5 most popular games on Steam to date.</h3>
-  <hr>
-  <h2>
-    <span class="name">${state.topalltime[0].name}</span>
-    <span class="developer">Developer: ${state.topalltime[0].developer}</span>
-    <span class="ccu">Average Player Count: ${state.topalltime[0].ccu}</span>
-  </h2>
-
-  <h2>
-    <span class="name">${state.topalltime[1].name}</span>
-    <span class="developer">Developer: ${state.topalltime[1].developer}</span>
-    <span class="ccu">Average Player Count: ${state.topalltime[1].ccu}</span>
-  </h2>
-  <h2>
-    <span class="name">${state.topalltime[2].name}</span>
-    <span class="developer">Developer: ${state.topalltime[2].developer}</span>
-    <span class="ccu">Average Player Count: ${state.topalltime[2].ccu}</span>
-  </h2>
-  <h2>
-    <span class="name">${state.topalltime[3].name}</span>
-    <span class="developer">Developer: ${state.topalltime[3].developer}</span>
-    <span class="ccu">Average Player Count: ${state.topalltime[3].ccu}</span>
-  </h2>
-  <h2>
-    <span class="name">${state.topalltime[4].name}</span>
-    <span class="developer">Developer: ${state.topalltime[4].developer}</span>
-    <span class="ccu">Average Player Count: ${state.topalltime[4].ccu}</span>
-  </h2>
-  <h6>*This list is updated once every 24 hours.</h6>
+<h1>
+If you are looking for some of the most popular games that Steam has, take a look at our list of the Top 5 most popular games on Steam to date.
+</h1>
+<h2>
+${state.topalltime[0].name}
+Developer: ${state.topalltime[0].developer}
+Average Player Count: ${state.topalltime[0].ccu}
+</h2>
+<h2>
+${state.topalltime[1].name}
+Developer: ${state.topalltime[1].developer}
+Average Player Count: ${state.topalltime[1].ccu}
+</h2>
+<h2>
+${state.topalltime[2].name}
+Developer: ${state.topalltime[2].developer}
+Average Player Count: ${state.topalltime[2].ccu}
+</h2>
+<h2>
+${state.topalltime[3].name}
+Developer: ${state.topalltime[3].developer}
+Average Player Count: ${state.topalltime[3].ccu}
+</h2>
+<h2>
+${state.topalltime[4].name}
+Developer: ${state.topalltime[4].developer}
+Average Player Count: ${state.topalltime[4].ccu}
+</h2>
 </section>
 `;
--- components/views/Singleplayer.js
+++ components/views/Singleplayer.js
@@ -1,60 +1,35 @@
 import html from "html-literal";
 
 export default (state) => html`
-  <section id="singleplayer">
-    <h1>Singleplayer</h1>
-    <h3>
-      If you are looking for some of the most popular singleplayer games that
-      Steam has, take a look at our list of the Top 5 most popular singleplayer
-      games in the last 24 hours.
-    </h3>
-    <hr />
-    <h2>
-      <span class="name">${state.topsingleplayer[0].name}</span>
-      <span class="developer"
-        >Developer: ${state.topsingleplayer[0].developer}</span
-      >
-      <span class="ccu"
-        >Average Player Count: ${state.topsingleplayer[0].ccu}</span
-      >
-    </h2>
-
-    <h2>
-      <span class="name">${state.topsingleplayer[1].name}</span>
-      <span class="developer"
-        >Developer: ${state.topsingleplayer[1].developer}</span
-      >
-      <span class="ccu"
-        >Average Player Count: ${state.topsingleplayer[1].ccu}</span
-      >
-    </h2>
-    <h2>
-      <span class="name">${state.topsingleplayer[2].name}</span>
-      <span class="developer"
-        >Developer: ${state.topsingleplayer[2].developer}</span
-      >
-      <span class="ccu"
-        >Average Player Count: ${state.topsingleplayer[2].ccu}</span
-      >
-    </h2>
-    <h2>
-      <span class="name">${state.topsingleplayer[3].name}</span>
-      <span class="developer"
-        >Developer: ${state.topsingleplayer[3].developer}</span
-      >
-      <span class="ccu"
-        >Average Player Count: ${state.topsingleplayer[3].ccu}</span
-      >
-    </h2>
-    <h2>
-      <span class="name">${state.topsingleplayer[4].name}</span>
-      <span class="developer"
-        >Developer: ${state.topsingleplayer[4].developer}</span
-      >
-      <span class="ccu"
-        >Average Player Count: ${state.topsingleplayer[4].ccu}</span
-      >
-    </h2>
-    <h6>*This list is updated once every 24 hours.</h6>
-  </section>
+<section id="singleplayer">
+  <h1>Singleplayer</h1>
+<h1>
+If you are looking for some of the most popular games that Steam has, take a look at our list of the Top 5 most popular games on Steam to date.
+</h1>
+<h2>
+${state.topsingleplayer[0].name}
+Developer: ${state.topsingleplayer[0].developer}
+Average Player Count: ${state.topsingleplayer[0].ccu}
+</h2>
+<h2>
+${state.topsingleplayer[1].name}
+Developer: ${state.topsingleplayer[1].developer}
+Average Player Count: ${state.topsingleplayer[1].ccu}
+</h2>
+<h2>
+${state.topsingleplayer[2].name}
+Developer: ${state.topsingleplayer[2].developer}
+Average Player Count: ${state.topsingleplayer[2].ccu}
+</h2>
+<h2>
+${state.topsingleplayer[3].name}
+Developer: ${state.topsingleplayer[3].developer}
+Average Player Count: ${state.topsingleplayer[3].ccu}
+</h2>
+<h2>
+${state.topsingleplayer[4].name}
+Developer: ${state.topsingleplayer[4].developer}
+Average Player Count: ${state.topsingleplayer[4].ccu}
+</h2>
+</section>
 `;
